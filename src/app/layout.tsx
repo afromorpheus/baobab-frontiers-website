27444b887a061bbf894627a7731ecb466b89db9a
afromorpheus <kabiru@nubianvr.com>
2025-08-13 21:27:50 +0000

    🔧 Fix Favicon 404 Error - Copied Brand Motif Ahoɔden SVG to public directory as favicon.svg - Updated all favicon references to use simpler filename - Fixed 404 errors that were preventing favicon display - Favicon should now display correctly in browser tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,11 +72,11 @@ export default function RootLayout() {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/svg+xml" href="/Brand%20Motif%20Aho%C9%94den%20-%20Coloured.svg" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/Brand%20Motif%20Aho%C9%94den%20-%20Coloured.svg" />
-        <link rel="icon" type="image/svg+xml" sizes="32x32" href="/Brand%20Motif%20Aho%C9%94den%20-%20Coloured.svg" />
-        <link rel="icon" type="image/svg+xml" sizes="16x16" href="/Brand%20Motif%20Aho%C9%94den%20-%20Coloured.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="32x32" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="16x16" href="/favicon.svg" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#007628" />
         <meta name="msapplication-TileColor" content="#007628" />
